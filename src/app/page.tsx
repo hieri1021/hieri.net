91d06e2fa6ca9d287827afcf5cb22a3ab39feaa1
hieri <mokkunjavelic1389@gmail.com>
2024-03-23 23:25:59 +0900

    Update website structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,51 +41,54 @@ export default function Home() {
     <div>
       <Head>
         <title>hieri.net</title>
-        <meta name="description" content="サイトもどき" />
+        <meta name="description" content="website" />
       </Head>
 
       <main className="container mx-auto">
-        <h1 className="text-4xl mt-3">ヒエリのサイト</h1>
+        <h1 className="text-5xl mt-3">hieri.net</h1>
 
-        <div className="text-5xl mt-10 mb-10">
-          <h1 className="text-center">
+        <div className="text-4xl mt-10 mb-10">
+          <h1 className="text-center mb-5">
             <a href="https://www.lovelive-anime.jp/otonokizaka/member/member02.html" target="_blank" rel="noopener noreferrer" className="underline decoration-blue-300">絢瀬絵里</a>
             を推せ！！！！！！！！！！
           </h1>
-          <h4 className="text-center">南條愛乃さんも推しましょう。約束。</h4>
-          <h1 className="text-right">
+          <h4 className="text-center mb-5">南條愛乃さんも推しましょう。約束。</h4>
+          <h1 className="text-center mb-5">
             南條さんが
-            <a href="https://www.dlsite.com/home/work/=/product_id/RJ01080844.html" target="_blank" rel="noopener noreferrer">ASMR作品</a>
+            <a href="https://www.dlsite.com/home/work/=/product_id/RJ01080844.html" target="_blank" rel="noopener noreferrer" className="underline decoration-blue-300">ASMR作品</a>
             を出してくださったぞ！！全人類聞け！！！！
           </h1>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2">
-          <div>
-            <img src="https://moe-counter-cf.yude.workers.dev/hieri"
-              alt="表示カウンター"
-              loading='lazy' />
-          </div>      
-
-          <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-end">
+          <div className='grid justify-items-center'>
             <img src="https://spotify-recently-played-readme.vercel.app/api?user=31upypeuqy2n3urisvkknr7gh2ra"
               alt="recently spotify"
-              loading='lazy' />
+              loading='lazy'
+              className='right-0' />
+          </div>
+          
+          <div className='grid justify-items-center lg:justify-items-end'>
+            <img src="https://moe-counter-cf.yude.workers.dev/hieri"
+              alt="表示カウンター"
+              loading='lazy'
+              className='w-1/2' />
           </div>
         </div>
         
         <p 
         id="RealtimeClockArea" suppressHydrationWarning={true}
-        className="text-right"
+        className="text-right text-xl"
         >
           {year}年{month}月{date}日{twoDigit(hours)}時{twoDigit(minutes)}分{twoDigit(seconds)}秒
         </p>
 
-        <hr />
-        <p style={{textAlign: "center"}}>
-          <FontAwesomeIcon icon={faGithub as IconDefinition} width={18} height={18} transform="down-1" /> Github:<a href="https://Github.com/hieri1021/hieri_website" target="_blank" rel="noopener noreferrer">hieri1021/hieri_website</a>. / <FontAwesomeIcon icon={faTwitter as IconDefinition} width={18} height={18} transform="down-1" /> Twitter:<a href="https://twitter.com/yamahi_5028" target="_blank" rel="noopener noreferrer">@yamahi_5028</a>
+        <hr className='mt-5' />
+        <img alt="hieribanner" src="./hieri.webp" className='h-12 inline' />
+        <p className='text-center text-xl'>
+          <FontAwesomeIcon icon={faGithub as IconDefinition} width={18} height={18} transform="down-1" /> Github : <a href="https://Github.com/hieri1021/hieri.net" target="_blank" rel="noopener noreferrer" className='underline decoration-blue-300'>hieri1021/hieri.net</a> / <FontAwesomeIcon icon={faTwitter as IconDefinition} width={18} height={18} transform="down-1" /> Twitter : <a href="https://twitter.com/yamahi_5028" target="_blank" rel="noopener noreferrer" className='underline decoration-blue-300'>@yamahi_5028</a> / Scrapbox : <a href="https://scrapbox.io/hieri-scrapbox" target="_blank" rel="noopener noreferrer" className='underline decoration-blue-300'>hieri-scrapbox</a>
         </p>
       </main>
     </div >    
-  )
-}
+    )
+  }
